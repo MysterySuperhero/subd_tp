0e3ea852792c1c54a94746624a45b6b0341f32a9
MysterySuperhero <nazarkovdmitri10@yandex.ru>
2015-10-31 19:29:04 +0300

    /forum/listUsers/ fix

--- app/forum.py
+++ app/forum.py
@@ -1,91 +1,90 @@
 from app import app
-from flask import jsonify, request
-from constants import *
+from flask import request
 from app.tools import forum, post, thread
 from app.tools import dbConnector
-import urlparse
 from app.tools import helpers
 import json
 
 
 @app.route('/db/api/forum/create/', methods=['POST'])
 def create_forum():
+    con = dbConnector.connect();
+    params = request.json;
 
-	con = dbConnector.connect();
-	params = request.json;
-	
-	try:
+    try:
 
-		helpers.check_params(params, ["name", "short_name", "user"])
-		response = forum.create(con, params["name"], params["short_name"], params["user"]);
+        helpers.check_params(params, ["name", "short_name", "user"])
+        response = forum.create(con, params["name"], params["short_name"], params["user"]);
 
-	except Exception as e:
-		con.close();
-		return json.dumps({"code": 3, "response": (e.message)})
-	
-	con.close();
-	return json.dumps({"code": 0, "response": response});
+    except Exception as e:
+        con.close();
+        return json.dumps({"code": 3, "response": (e.message)})
+
+    con.close();
+    return json.dumps({"code": 0, "response": response});
 
 
 @app.route('/db/api/forum/details/', methods=['GET'])
 def forum_details():
-	con = dbConnector.connect();
-	params = helpers.json_from_get(request)
-	try:
-		helpers.check_params(params, ["forum"])
-		response = forum.details(con, params["forum"], helpers.related_exists(params))
-	except Exception, e:
-		con.close;
-		return json.dumps({"code": 3, "response": (e.message)})
-	con.close();
-	return json.dumps({"code": 0, "response": response});
+    con = dbConnector.connect();
+    params = helpers.json_from_get(request)
+    try:
+        helpers.check_params(params, ["forum"])
+        response = forum.details(con, params["forum"], helpers.related_exists(params))
+    except Exception, e:
+        con.close;
+        return json.dumps({"code": 3, "response": (e.message)})
+    con.close();
+    return json.dumps({"code": 0, "response": response});
+
 
 @app.route('/db/api/forum/listPosts/', methods=['GET'])
 def forum_list_posts():
-	con = dbConnector.connect()
-	params = helpers.json_from_get(request)
-	optional = helpers.get_optional_params(
-		params,
-		["since",
-		"limit",
-		"order"]
-	)
-	related = helpers.related_exists(params)
-	try:
-		helpers.check_params(params, ["forum"])
-		response = post.posts_list(con, entity = "forum", params=optional, identifier=params["forum"], related=related)
-	except Exception, e:
-		con.close;
-		return json.dumps({"code": 3, "response": (e.message)})
-	con.close();
-	return json.dumps({"code": 0, "response": response});
+    con = dbConnector.connect()
+    params = helpers.json_from_get(request)
+    optional = helpers.get_optional_params(
+        params,
+        ["since",
+         "limit",
+         "order"]
+    )
+    related = helpers.related_exists(params)
+    try:
+        helpers.check_params(params, ["forum"])
+        response = post.posts_list(con, entity="forum", params=optional, identifier=params["forum"], related=related)
+    except Exception, e:
+        con.close;
+        return json.dumps({"code": 3, "response": (e.message)})
+    con.close();
+    return json.dumps({"code": 0, "response": response});
+
 
 @app.route('/db/api/forum/listThreads/', methods=['GET'])
 def forum_list_threads():
-	con = dbConnector.connect()
-	params = helpers.json_from_get(request)
-	optional = helpers.get_optional_params(params, ["since", "limit", "order"])
-	related = helpers.related_exists(params)
-	try:
-		helpers.check_params(params, ["forum"])
-		response = thread.list(con=con, optional=optional, required=params, related=related)
-	except Exception, e:
-		con.close;
-		return json.dumps({"code": 3, "response": (e.message)})
-	con.close();
-	return json.dumps({"code": 0, "response": response});
+    con = dbConnector.connect()
+    params = helpers.json_from_get(request)
+    optional = helpers.get_optional_params(params, ["since", "limit", "order"])
+    related = helpers.related_exists(params)
+    try:
+        helpers.check_params(params, ["forum"])
+        response = thread.list(con=con, optional=optional, required=params, related=related)
+    except Exception, e:
+        con.close;
+        return json.dumps({"code": 3, "response": (e.message)})
+    con.close();
+    return json.dumps({"code": 0, "response": response});
 
 
 @app.route('/db/api/forum/listUsers/', methods=['GET'])
 def forum_list_users():
-	con = dbConnector.connect()
-	params = helpers.json_from_get(request)
-	optional = helpers.get_optional_params(params, ["since_id", "limit", "order"])
-	try:
-		helpers.check_params(params, ["forum"])
-		response = forum.forum_listUsers(con=con, optional=optional, forum_shortname=params["forum"][0])
-	except Exception, e:
-		con.close;
-		return json.dumps({"code": 3, "response": (e.message)})
-	con.close();
-	return json.dumps({"code": 0, "response": response});
+    con = dbConnector.connect()
+    params = helpers.json_from_get(request)
+    optional = helpers.get_optional_params(params, ["since_id", "limit", "order"])
+    try:
+        helpers.check_params(params, ["forum"])
+        response = forum.forum_listUsers(con=con, optional=optional, forum_shortname=params["forum"][0])
+    except Exception, e:
+        con.close;
+        return json.dumps({"code": 3, "response": (e.message)})
+    con.close();
+    return json.dumps({"code": 0, "response": response});
--- app/tools/forum.py
+++ app/tools/forum.py
@@ -1,60 +1,74 @@
 from app.tools import dbConnector
 from app.tools import user
 
+
 def create(con, name, short_name, user):
+    dbConnector.update_query(
+        con,
+        'INSERT INTO forum (name, short_name, user) VALUES (%s, %s, %s)', (name, short_name, user)
+    );
 
-	dbConnector.update_query(
-		con, 
-		'INSERT INTO forum (name, short_name, user) VALUES (%s, %s, %s)', (name, short_name, user)
-	);
+    # check result of update query:
+    forum = dbConnector.select_query(
+        con,
+        'SELECT id, name, short_name, user FROM forum WHERE short_name = %s', (short_name,)
+    )
 
-	#check result of update query:
-	forum = dbConnector.select_query(
-		con,
-		'SELECT id, name, short_name, user FROM forum WHERE short_name = %s', (short_name, )
-	)
+    return forum_description(forum);
 
-	return forum_description(forum);
 
 def details(con, short_name, related):
-	forum = dbConnector.select_query(
-		con, 'SELECT id, name, short_name, user FROM forum WHERE short_name = %s', (short_name, )
-	)
+    forum = dbConnector.select_query(
+        con, 'SELECT id, name, short_name, user FROM forum WHERE short_name = %s', (short_name,)
+    )
 
-	if len(forum) == 0:
-		raise Exception("Forum " + short_name + " not found")
+    if len(forum) == 0:
+        raise Exception("Forum " + short_name + " not found")
 
-	forum = forum_description(forum)
+    forum = forum_description(forum)
 
-	if "user" in related:
-		forum["user"] = user.details(con, forum["user"])
-	return forum
+    if "user" in related:
+        forum["user"] = user.details(con, forum["user"])
+    return forum
+
 
 def forum_listUsers(con, forum_shortname, optional):
-	print "______LAST MAFAKA_______"
-	query = "SELECT user FROM post WHERE forum = \'" + str(forum_shortname) + "\' GROUP BY user" 
-	print query
+    # query = "SELECT user FROM post WHERE forum = \'" + str(forum_shortname) + "\' GROUP BY user"
 
-	try:
-		posts = dbConnector.select_query(con, query, ())
-	except Exception as e:
-		print (e.message)
+    print("UUUUU")
+    query = "SELECT user.id, user.name, user.email FROM user " \
+        "WHERE user.email IN (SELECT DISTINCT user FROM post WHERE forum = \'" + str(forum_shortname) + "\')"
+    if "since_id" in optional:
+        query += " AND user.id >= " + str(optional["since_id"][0])
+    if "order" in optional:
+        query += " ORDER BY user.name " + str(optional["order"][0])
+    if "limit" in optional:
+        query += " LIMIT " + str(optional["limit"][0])
+    print query
 
-	print posts
+    try:
+        posts = dbConnector.select_query(con, query, ())
+    except Exception as e:
+        print (e.message)
 
-	response = []
-	for post in posts:
-		print str(post)
-		res = user.details(con, str(post[0]))
-		print res
-		response.append(res)
-	# query = "SELECT about, email, id, isAnonymous, name, username FROM user"
-	print response
+    print posts
 
-	return response
+    response = []
+    for post in posts:
+        print("POST:")
+        print str(post)
+        print("USER TO FOUND: " + str(post[2]))
+        res = user.details(con, str(post[2]))
+        print res
+        response.append(res)
+    print("AZAZAZA")
+    print response
+    print(len(response))
+
+    return response
+
 
 def forum_description(forum):
-    
     forum = forum[0]
     response = {
         'id': forum[0],
